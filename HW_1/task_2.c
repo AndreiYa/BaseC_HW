c6772af21a16d522cb7104fb2d56b7ab61fd1246
AndreiYa <36473307+AndreiYa@users.noreply.github.com>
2024-04-06 08:21:10 +0300

    fix

--- HW_1/task_2.c
+++ HW_1/task_2.c
@@ -3,8 +3,8 @@
 int main(void)
 {
 	printf("Let`s\n");
-	printf("\tgo\n");
-	printf("\t\tto walk");
+	printf("   go\n");
+	printf("     to walk");
 	return 0;
 }
 
--- HW_1/task_3.c
+++ HW_1/task_3.c
@@ -6,7 +6,7 @@ int main(void)
 	printf("  ***\n");
 	printf(" *****\n");
 	printf("*******\n");
-	printf(" HH HH\n");
+	printf(" HHOHH\n");
 	printf(" ZZZZZ\n");
 	return 0;
 }
